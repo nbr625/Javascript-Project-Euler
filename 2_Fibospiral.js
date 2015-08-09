04b7686baf88a27b8d34e0d873401f9a3dd11893
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-08-09 17:40:11 -0400

    added descriptions to the problems

--- 2_Fibospiral.js
+++ 2_Fibospiral.js
@@ -1,3 +1,9 @@
+//If we list all the natural numbers below 10 that are multiples of 3 or 5,
+//we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+//1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+//By considering the terms in the Fibonacci sequence 
+//whose values do not exceed four million, find the sum of the even-valued terms.
 function fibbonize(sum, i) {
 	if( i == 0){
 		return 1;
--- 3_optimus_prime.js
+++ 3_optimus_prime.js
@@ -1,3 +1,7 @@
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//What is the largest prime factor of the number 600851475143 ?
+
+
 function optimus_prime(n) {
 	var max = Math.round(Math.sqrt(n));
 	for(var i = max; i >= 2; i --) {
